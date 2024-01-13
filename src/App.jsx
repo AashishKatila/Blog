01888a3ee1b9a21eff0043a0784ceaef8f0613a3
Aashish Katila <aashishkatila@gmail.com>
2024-01-13 19:13:28 +0545

    copied navbar content back to app.jsx for ease and added signout function

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
-import React, { useState } from 'react'
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home'
-import CreatePost from './pages/CreatePost'
-import Login from './pages/Login'
-import Navbar from './components/Navbar'
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
+import { signOut } from 'firebase/auth'
+import {auth} from './firebase-config'
 
-const App =() => {
-  const [isAuth,setIsAuth] = useState(false)
-  return(
+const App = () => {
+  const [isAuth, setIsAuth] = useState(false);
+
+  const signUserOut = () =>{
+    signOut(auth).then(() =>{
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname ="/login"
+  })
+  }
+
+  return (
     <Router>
-      <Navbar />
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/createpost">Createpost</Link>
+        {!isAuth ? (
+          <Link to="/login">Login</Link>
+        ) : (
+          <button onClick={signUserOut}>Sign Out</button>
+        )}
+      </nav>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/createpost' element={<CreatePost />} />
-        <Route path='/login' element={<Login setIsAuth={setIsAuth}/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
