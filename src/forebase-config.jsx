65945031c598df5fb243d95e64dd50d675dfad82
Aashish Katila <aashishkatila@gmail.com>
2024-01-13 16:01:17 +0545

    Added firebase,react-router-dom packages and configured firebase

---
+++ src/forebase-config.jsx
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvfwXYtIsmGzu74nldhriOxyc9-3KKB0k",
+  authDomain: "blogproject-7d2c9.firebaseapp.com",
+  projectId: "blogproject-7d2c9",
+  storageBucket: "blogproject-7d2c9.appspot.com",
+  messagingSenderId: "49376359424",
+  appId: "1:49376359424:web:95ec35f246c47887f094e5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
