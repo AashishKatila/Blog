4f0aa8277a375591391d83cb0cdfccc71b80288b
Aashish Katila <aashishkatila@gmail.com>
2024-01-13 16:23:02 +0545

    Added pages and routes

--- src/forebase-config.jsx
+++ src/forebase-config.jsx
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider} from "firebase/auth";
+import { getFirestore} from 'firebase/firestore'
 
 // Firebase configuration
 const firebaseConfig = {
@@ -12,3 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app)
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
