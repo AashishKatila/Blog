017d024bd70839b0822666f573bb55f572160a85
Aashish Katila <aashishkatila@gmail.com>
2024-01-13 17:42:34 +0545

    added useNavigate to redirect to homepage

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import {auth,provider} from '../forebase-config'
 import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
 
 const Login = ({setIsAuth}) => {
+  const navigate = useNavigate()
   const signInWithGoogle = () =>{
     signInWithPopup(auth,provider).then((result) =>{
       localStorage.setItem("isAuth",true)
       setIsAuth(true)
+      navigate('/')
     })
   }
   return (
