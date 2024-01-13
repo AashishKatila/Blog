dce6fb01d266b402b98913cad5320ebc41060f0e
Aashish Katila <aashishkatila@gmail.com>
2024-01-13 19:20:28 +0545

    pasted the css for login page

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
     <Router>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/createpost">Createpost</Link>
+        <Link to="/createpost">Create Post</Link>
         {!isAuth ? (
           <Link to="/login">Login</Link>
         ) : (
