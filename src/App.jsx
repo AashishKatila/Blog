a47d1b5964d9920a3332dffb3ad3bbb2e8bade07
Aashish Katila <aashishkatila@gmail.com>
2024-01-13 21:22:33 +0545

    Posts can be created

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,43 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Login from "./pages/Login";
-import { signOut } from 'firebase/auth'
-import {auth} from './firebase-config'
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
 
 const App = () => {
   const [isAuth, setIsAuth] = useState(false);
 
-  const signUserOut = () =>{
-    signOut(auth).then(() =>{
+  const signUserOut = () => {
+    signOut(auth).then(() => {
       localStorage.clear();
       setIsAuth(false);
-      window.location.pathname ="/login"
-  })
-  }
+      window.location.pathname = "/login";
+    });
+  };
 
   return (
-    <div className="App">
     <Router>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/createpost">Create Post</Link>
+
         {!isAuth ? (
           <Link to="/login">Login</Link>
         ) : (
-          <button onClick={signUserOut}>Sign Out</button>
+          <>
+            <Link to="/createpost">Create Post</Link>
+            <button onClick={signUserOut}>Sign Out</button>
+          </>
         )}
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/createpost" element={<CreatePost isAuth={isAuth} />} />
         <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
       </Routes>
     </Router>
-    </div>
   );
 };
 
--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,22 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { db, auth } from "../firebase-config";
+import { addDoc, collection } from "firebase/firestore";
+import {useNavigate} from 'react-router-dom'
 
-const CreatePost = () => {
+const CreatePost = ({isAuth}) => {
+  const [title, setTitle] = useState("");
+  const [post, setPost] = useState("");
+
+  const postsCollectionRef = collection(db, "posts");
+  let navigate = useNavigate()
+
+  const handlePost = async () => {
+    await addDoc(postsCollectionRef, {
+      title,
+      post,
+      author: 
+      { 
+        name: auth.currentUser.displayName, 
+        id: auth.currentUser.uid 
+      },
+    });
+    navigate('/')
+  };
+
+  useEffect(() =>{
+    if(!isAuth) navigate("/login")
+  },[])
+
   return (
-    <div className='createPostPage' style={{display:"flex",justifyContent:"center",paddingTop:"30px"}}>
+    <div
+      className="createPostPage"
+      style={{ display: "flex", justifyContent: "center", paddingTop: "30px" }}
+    >
       <div className="createPostContainer">
         <h1>Create A Post</h1>
         <div className="inputDiv">
           <label>Title: </label>
-          <input placeholder='Title...'/>
+          <input
+            placeholder="Title..."
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+          />
         </div>
         <div className="inputDiv">
           <label>Post: </label>
-          <textarea placeholder='Title...'/>
+          <textarea
+            placeholder="Post..."
+            onChange={(event) => {
+              setPost(event.target.value);
+            }}
+          />
         </div>
-        <button>Submit Post</button>
+        <button onClick={handlePost}>Submit Post</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreatePost
+export default CreatePost;
