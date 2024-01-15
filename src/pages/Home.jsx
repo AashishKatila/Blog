b0c18b7c69e62c269d87a61fb09f09168ade64dc
Aashish Katila <aashishkatila@gmail.com>
2024-01-15 15:09:44 +0545

    Added deletePost

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import { collection, getDocs } from 'firebase/firestore'
+import React, { useEffect, useState } from "react";
+import { collection, deleteDoc, getDocs, doc } from "firebase/firestore";
 import { auth, db } from "../firebase-config";
-import '../App.css'
+import "../App.css";
 
-const Home = () => {
-  const [postLists,setPostLists] = useState([])
-  const postsCollectionRef = collection(db,"posts")
+const Home = ({isAuth}) => {
+  const [postLists, setPostLists] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
 
-
-  useEffect(()=>{
-    const getPosts = async() =>{
+  useEffect(() => {
+    const getPosts = async () => {
       const data = await getDocs(postsCollectionRef);
       // console.log(data.docs);
-      setPostLists(data.docs.map((doc) => ({...doc.data(),id: doc.id})))
+      setPostLists(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
       console.log(postLists);
-    }
+    };
     getPosts();
-  },[])
+  },[]);
+
+  const deletePost = async (id) => {
+    const postDoc = doc(db, "posts", id);
+    await deleteDoc(postDoc);
+  };
+
   return (
-    <div className='homePage'>
-      {postLists.map((post) =>{
-        return(
+    <div className="homePage">
+      {postLists.map((post) => {
+        return (
           <div className="post">
             <div className="postHeader">
               <div className="title">
                 <h1>{post.title}</h1>
               </div>
+              <div className="deletePost">
+                {isAuth && 
+                post.author.id === auth.currentUser.uid && (
+                  <button onClick={() => deletePost(post.id)}>
+                    Delete
+                  </button>
+                )}
+              </div>
             </div>
-            <div className="postTextContainer">
-              {post.post}
-            </div>
+            <div className="postTextContainer">{post.post}</div>
             <h3>@{post.author.name}</h3>
           </div>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
