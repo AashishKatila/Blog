f12381410a613ef0cbd8a245f49a4c9d8b76fd4e
Aashish Katila <aashishkatila@gmail.com>
2024-01-15 14:31:23 +0545

    Displayed the Post on Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { collection, getDocs } from 'firebase/firestore'
+import { auth, db } from "../firebase-config";
 
 const Home = () => {
+  const [postLists,setPostLists] = useState([])
+  const postsCollectionRef = collection(db,"posts")
+
+
+  useEffect(()=>{
+    const getPosts = async() =>{
+      const data = await getDocs(postsCollectionRef);
+      // console.log(data.docs);
+      setPostLists(data.docs.map((doc) => ({...doc.data(),id: doc.id})))
+      console.log(postLists);
+    }
+    getPosts();
+  },[])
   return (
-    <div>Home</div>
+    <div className='homePage'>
+      {postLists.map((post) =>{
+        return(
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h1>{post.title}</h1>
+              </div>
+            </div>
+            <div className="postTextContainer">
+              {post.post}
+            </div>
+            <h3>@{post.author.name}</h3>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
